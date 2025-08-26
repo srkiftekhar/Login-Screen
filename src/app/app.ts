d64d42c16f3fd19d27779c9a6e93e91c8158dff6
srkiftekhar <shahrukh099aa@gmail.com>
2025-08-26 18:31:18 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('login-screen');
-}
+export class App {}
--- src/app/login-screen/login-screen.ts
+++ src/app/login-screen/login-screen.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-screen.html',
-  styleUrl: './login-screen.css'
+  styleUrl: './login-screen.css',
 })
 export class LoginScreen {
+  email: string = '';
+  password = '';
 
+  emailReg = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+  passwrdReg = /^[0-9]{6}$/;
+
+  constructor(private router: Router) {}
+
+  loginHandler() {
+    if (!this.email) {
+      alert('Enter the Email');
+      return;
+    } else if (!this.emailReg.test(this.email)) {
+      alert('Enter a valid Email!');
+      return;
+    }
+
+    if (!this.password) {
+      alert('Enter the Password');
+      return;
+    } else if (!this.passwrdReg.test(this.password)) {
+      alert('Password must be exactly 6 digits!');
+      return;
+    }
+
+    if (this.emailReg.test(this.email) && this.passwrdReg.test(this.password)) {
+      alert('login Successfull');
+    }
+    this.router.navigate(['/home']);
+  }
+
+  protected readonly title = signal('login-screen');
 }
