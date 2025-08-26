09f666e9ed19e9f0c10f443299f0679f7d42f633
srkiftekhar <shahrukh099aa@gmail.com>
2025-08-26 13:29:40 +0530

    Initial commit - Login Screen

---
+++ src/app/login-screen/login-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-screen',
+  imports: [],
+  templateUrl: './login-screen.html',
+  styleUrl: './login-screen.css'
+})
+export class LoginScreen {
+
+}
